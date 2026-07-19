ae77967d43ee59763bceed8a41586e2bec7a57bc
Morgan <morganfairbank67@gmail.com>
2026-07-19 17:52:00 +0100

    Tested validate.py on raw data from extact.py and successfully validates. Now load email and password from env using dotenv.

--- Scraper/extract.py
+++ Scraper/extract.py
@@ -1,35 +1,41 @@
 import re
+import os
 
-from playwright.sync_api import sync_playwright
-import time
+from playwright.sync_api import sync_playwright, Page, BrowserContext
 import datetime
+from datetime import date
+from dotenv import load_dotenv
 
-def login_Save_State(page, context):
-    YourEmail = ""
-    YourPassword = ""
+load_dotenv()
 
+def login(page: Page):
+    email = os.getenv("ROTRENDS_EMAIL")
+    password = os.getenv("ROTRENDS_PASSWORD")
+
+    if not email or not password:
+        raise ValueError("ROTRENDS_EMAIL and ROTRENDS_PASSWORD environment variables must be set.")
+    
     page.goto("https://rotrends.com/login")
 
     email_box = page.get_by_role("textbox", name="Email Address")
     email_box.wait_for(state="visible")
-
     email_box.click()
-    time.sleep(1)
-    email_box.press_sequentially(YourEmail, delay=300)
+
+    page.wait_for_timeout(500)
+    email_box.press_sequentially(email, delay=100)
     
     page.keyboard.press("Tab")
     page.wait_for_timeout(500)
     
-    page.keyboard.insert_text(YourPassword)
+    page.keyboard.insert_text(password)
     page.wait_for_timeout(500)
 
-    
     page.keyboard.press("Enter")
-    
-    time.sleep(5)
 
-    context.storage_state(path="auth_state.json")
-    print("Login successful and state saved.")
+    page.wait_for_url("https://rotrends.com/**", timeout=15000)
+    page.wait_for_timeout(1000)
+    
+    print("Login successful.")
 
 def set_preferences(page, columns):
     page.get_by_role("button").filter(has_text="Metrics").click()
@@ -59,84 +65,69 @@ def extract_games(page, category):
     games = page.query_selector_all(".ant-table-row")
 
     for game in games:
-        cells = game.query_selector_all(".ant-table-cell")
-        game_data = {}
-        game_data["Category"] = category
-        for i in range(len(cells)):
-            if i < len(headers):
-                column_name = headers[i]
+        try:
+            cells = game.query_selector_all(".ant-table-cell")
+            game_data = {}
+            game_data["Category"] = category
+            for i in range(len(cells)):
+                if i < len(headers):
+                    column_name = headers[i]
 
-                if column_name == "":
-                    continue
-                    
-                cell_value = cells[i].inner_text().strip()
-                game_data[column_name] = cell_value
-        game_data["Date Extracted"] = datetime.datetime.now().strftime("%Y-%m-%d")
-        all_games.append(game_data)
-
+                    if column_name == "":
+                        continue
+                        
+                    cell_value = cells[i].inner_text().strip()
+                    game_data[column_name] = cell_value
+            game_data["Date Extracted"] = datetime.datetime.now().strftime("%Y-%m-%d")
+            all_games.append(game_data)
+        except Exception as e:
+            print(f"Error scraping game: {e}")
+            continue
 
     return all_games
 
-
-def new_and_rising_games(page, columns):
-     page.goto("https://rotrends.com/games?page=1&page_size=100&sort=-rank_change_day&created_min=2026-06-04T00%3A00%3A00.000Z")
-     set_preferences(page, columns)
-     return extract_games(page, "New and Rising")
-
-def top_moving_games(page, columns):
-     page.goto("https://rotrends.com/games?page=1&page_size=100&sort=-rank_change_day&playing_min=500")
-     set_preferences(page, columns)
-     return extract_games(page, "Top Moving")
-
-def new_and_stable_games(page, columns):
-     page.goto("https://rotrends.com/games?page=1&page_size=100&sort=-rap_14d&created_min=2026-06-05T00%3A00%3A00.000Z")
-     set_preferences(page, columns)
-     return extract_games(page, "New and Stable")
-
-def top_100_games(page, columns):
-        page.goto("https://rotrends.com/games?page=1&page_size=100&sort=-rank_change_day&rank_max=100")
-        set_preferences(page, columns)
-        return extract_games(page, "Top 100")
-
-def top_by_earning(page, columns):
-     page.goto("https://rotrends.com/games?page=1&page_size=100&sort=-earning_rank")
-     set_preferences(page, columns)
-     return extract_games(page, "Top by Earning")
-
-def top_by_players(page, columns):
-     page.goto("https://rotrends.com/games?page=1&page_size=100&sort=-players")
-     set_preferences(page, columns)
-     return extract_games(page, "Top by Players")
-
-def top_by_session(page, columns):
-     page.goto("https://rotrends.com/games?page=1&page_size=100&sort=-est_avg_session_duration_naive&playing_min=100")
-     set_preferences(page, columns)
-     return extract_games(page, "Top by Session")
+def scrape_category(page: Page, category: str, url: str, columns: list[str]) -> list[dict]:
+    """Navigates to a category URL, sets preferences, and extracts game data."""
+    print(f"Scraping category: {category}...")
+    page.goto(url)
+    set_preferences(page, columns)
+    return extract_games(page, category)
 
 def main():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)
-
+        
+        # Force a fresh login every time
         context = browser.new_context()
         page = context.new_page()
+        login(page)
 
         columns = ["Game Name", "Global Rank", "Global Rank Shift (1d)", "Global Rank Shift (1w)", "Global Rank Shift (1m)", "Avg CCU Rank (1d)", "Avg CCU Rank (7d)", "Avg CCU Rank (14d)", "Avg CCU Rank Shift (1d)", "Earning Rank", "Genre", "Sub Genre", "Visits", "Players (CCU)", "Platform Share", "Avg CCU (1d)", "Avg CCU (7d)", "Avg CCU (14d)", "Momentum (1d)", "Momentum (1w)", "Momentum (1m)", "Avg Session", "Favorites", "Rating", "Up Votes", "Down Votes", "Created", "Extract Date"]
 
-        login_Save_State(page, context)
+        from dateutil.relativedelta import relativedelta
 
-        all_games_final = []
+        dateToday = date.today().strftime("%Y-%m-%d")
+        dateOneMonthAgo = (date.today() - relativedelta(months=1)).strftime("%Y-%m-%d")
 
-        all_games_final.append(new_and_rising_games(page, columns))
-        all_games_final.append(top_moving_games(page, columns))
-        all_games_final.append(new_and_stable_games(page, columns))
-        all_games_final.append(top_100_games(page, columns))
-        all_games_final.append(top_by_earning(page, columns))
-        all_games_final.append(top_by_players(page, columns))
-        all_games_final.append(top_by_session(page, columns))
+        categories_to_scrape = {
+            "Top Moving": "https://rotrends.com/games?page=1&page_size=100&sort=-rank_change_day&playing_min=500",
+            "New and Rising": f"https://rotrends.com/games?page=1&page_size=100&sort=-rank_change_day&created_min={dateOneMonthAgo}T00%3A00%3A00.000Z",
+            "New and Stable": f"https://rotrends.com/games?page=1&page_size=100&sort=-rap_14d&created_min={dateOneMonthAgo}T00%3A00%3A00.000Z",
+            "Top 100": "https://rotrends.com/games?page=1&page_size=100&sort=-rank_change_day&rank_max=100",
+            "Top by Earning": "https://rotrends.com/games?page=1&page_size=100&sort=-earning_rank",
+            "Top by Players": "https://rotrends.com/games?page=1&page_size=100&sort=-players",
+            "Top by Session": "https://rotrends.com/games?page=1&page_size=100&sort=-est_avg_session_duration_naive&playing_min=100"
+        }
+
+        all_games_data = []
+        for category, url in categories_to_scrape.items():
+            games = scrape_category(page, category, url, columns)
+            all_games_data.extend(games) # Flatten the list
 
         browser.close()
-
-        return all_games_final
+        print(f"Scraping complete. Extracted data for {len(all_games_data)} games.")
+        print(all_games_data[1])
+        return all_games_data
 
 if __name__ == "__main__":
     main()
--- Scraper/validate.py
+++ Scraper/validate.py
@@ -1,40 +1,42 @@
 import re
 
-from pydantic import BaseModel, Field, field_validator, model_validator
+from pydantic import BaseModel, Field, field_validator, model_validator, ValidationError
 from typing import Any, Optional
 from datetime import datetime
 import uuid
 
+from extract import main
+
 class ScrapedGameData(BaseModel):
     game_id: uuid.UUID = Field(default_factory=lambda: uuid.uuid4(), description="Unique identifier for the game")
-    game_name: str = Field(..., description="Name of the game")
-    category: str = Field(..., description="Category of the game")
-    global_rank: int = Field(None, description="Global rank of the game")
-    global_rank_shift_1d: int = Field(None, description="Global rank shift in the last 1 day")
-    global_rank_shift_1w: int = Field(None, description="Global rank shift in the last 1 week")
-    global_rank_shift_1m: int = Field(None, description="Global rank shift in the last 1 month")
-    avg_ccu_rank_7d: int = Field(None, description="Average CCU rank over the last 7 days")
-    avg_ccu_rank_14d: int = Field(None, description="Average CCU rank over the last 14 days")
-    avg_ccu_rank_shift_1d: int = Field(None, description="Average CCU rank shift in the last day")
-    earning_rank: Optional[int] = Field(None, description="Earning rank of the game")
-    genre: str = Field(None, description="Genre of the game")
-    sub_genre: Optional[str] = Field(None, description="Sub-genre of the game")
-    visits: Optional[int] = Field(None, description="Number of visits")
-    players_ccu: int = Field(None, description="Current Concurrent Users (CCU)")
-    platform_share: float = Field(None, description="Platform share percentage")
-    avg_ccu_1d: int = Field(None, description="Average CCU over the last 1 day")
-    avg_ccu_7d: int = Field(None, description="Average CCU over the last 7 days")
-    avg_ccu_14d: int = Field(None, description="Average CCU over the last 14 days")
-    momentum_1d: Optional[str] = Field(None, description="Momentum over the last 1 day")
-    momentum_1w: Optional[str] = Field(None, description="Momentum over the last 1 week")
-    momentum_1m: Optional[str] = Field(None, description="Momentum over the last 1 month")
-    avg_session: str = Field(None, description="Average session duration")
-    favorites: int = Field(None, description="Number of favorites")
-    rating: float = Field(None, description="Rating of the game")
-    up_votes: int = Field(None, description="Number of up votes")
-    down_votes: int = Field(None, description="Number of down votes")
-    created: datetime = Field(None, description="Creation date of the game")
-    extract_date: datetime = Field(None, description="Date when the data was extracted")
+    game_name: str = Field(...,alias="Games", description="Name of the game")
+    category: str = Field(...,alias="Category", description="Category of the game")
+    global_rank: Optional[int] = Field(None,alias="Global Rank", description="Global rank of the game")
+    global_rank_shift_1d: Optional[int] = Field(None,alias="Global Rank Shift (1d)", description="Global rank shift in the last 1 day")
+    global_rank_shift_1w: Optional[int] = Field(None,alias="Global Rank Shift (1w)", description="Global rank shift in the last 1 week")
+    global_rank_shift_1m: Optional[int] = Field(None,alias="Global Rank Shift (1m)", description="Global rank shift in the last 1 month")
+    avg_ccu_rank_7d: Optional[int] = Field(None,alias="Avg CCU Rank (7d)", description="Average CCU rank over the last 7 days")
+    avg_ccu_rank_14d: Optional[int] = Field(None,alias="Avg CCU Rank (14d)", description="Average CCU rank over the last 14 days")
+    avg_ccu_rank_shift_1d: Optional[int] = Field(None,alias="Avg CCU Rank Shift (1d)", description="Average CCU rank shift in the last day")
+    earning_rank: Optional[int] = Field(None,alias="Earning Rank", description="Earning rank of the game")
+    genre: Optional[str] = Field(None,alias="Genre", description="Genre of the game")
+    sub_genre: Optional[str] = Field(None,alias="Sub Genre", description="Sub-genre of the game")
+    visits: Optional[int] = Field(None,alias="Visits", description="Number of visits")
+    players_ccu: int = Field(None,alias="Players (CCU)", description="Current Concurrent Users (CCU)")
+    platform_share: float = Field(None,alias="Platform Share", description="Platform share percentage")
+    avg_ccu_1d: Optional[int] = Field(None,alias="Avg CCU (1d)", description="Average CCU over the last 1 day")
+    avg_ccu_7d: Optional[int] = Field(None,alias="Avg CCU (7d)", description="Average CCU over the last 7 days")
+    avg_ccu_14d: Optional[int] = Field(None,alias="Avg CCU (14d)", description="Average CCU over the last 14 days")
+    momentum_1d: Optional[str] = Field(None,alias="Momentum (1d)", description="Momentum over the last 1 day")
+    momentum_1w: Optional[str] = Field(None,alias="Momentum (1w)", description="Momentum over the last 1 week")
+    momentum_1m: Optional[str] = Field(None,alias="Momentum (1m)", description="Momentum over the last 1 month")
+    avg_session: Optional[str] = Field(None,alias="Avg Session", description="Average session duration")
+    favorites: Optional[int] = Field(None,alias="Favorites", description="Number of favorites")
+    rating: Optional[float] = Field(None,alias="Rating", description="Rating of the game")
+    up_votes: Optional[int] = Field(None,alias="Up Votes", description="Number of up votes")
+    down_votes: Optional[int] = Field(None,alias="Down Votes", description="Number of down votes")
+    created: Optional[datetime] = Field(None,alias="Created", description="Creation date of the game")
+    extract_date: datetime = Field(None,alias="Date Extracted", description="Date when the data was extracted")
 
     @model_validator(mode="before")
     @classmethod
@@ -49,7 +51,7 @@ class ScrapedGameData(BaseModel):
     
     @field_validator(
         'global_rank', 'global_rank_shift_1d', 'global_rank_shift_1w', 'global_rank_shift_1m',
-        'avg_ccu_rank_7d', 'avg_ccu_rank_14d', 'avg_ccu_rank_shift_1m', 'earning_rank', mode='before'
+        'avg_ccu_rank_7d', 'avg_ccu_rank_14d', 'avg_ccu_rank_shift_1d', 'earning_rank', mode='before'
     )
     @classmethod
     def parse_ranks(cls, value: Any) -> Any:
@@ -57,6 +59,8 @@ class ScrapedGameData(BaseModel):
         if isinstance(value, str):
             # Remove commas, plus signs, and hashtags
             val = value.replace(',', '').replace('+', '').replace('#', '').strip()
+            if '\n' in val:
+                val = val.split('\n')[0]
             try:
                 return int(val)
             except ValueError:
@@ -104,3 +108,17 @@ class ScrapedGameData(BaseModel):
              except ValueError:
                  pass
         return value
+
+if __name__ == "__main__":
+    print("Running Scraper...")
+    raw_data = main()
+
+    successCount = 0
+    for row in raw_data:
+        try:
+            validated = ScrapedGameData(**row)
+            successCount += 1
+        except ValidationError as e:
+            print(f"Validation error: {e}")
+    
+    print(f"Scraped {len(raw_data)} games, {successCount} games validated.")
